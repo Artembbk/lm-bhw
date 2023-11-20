b6285caf5c3e8569326bba2dc4722449d13ff1ed
Artem Babak <artbbk27@gmail.com>
2023-11-20 15:24:15 +0300

    rea

--- collate/collate.py
+++ collate/collate.py
@@ -2,7 +2,10 @@ import numpy as np
 import torch
 
 def collate_fn(data):
-    sequences = torch.stack([torch.from_numpy(pair[0]) for pair in data])
+    sequences = [torch.tensor(pair[0], dtype=torch.int64) for pair in data]
+
+    # Если хотите объединить массивы в один longtensor, используйте torch.cat
+    sequences = torch.cat(sequences)
     lengths = torch.tensor([pair[1] for pair in data])
     return sequences, lengths
 
