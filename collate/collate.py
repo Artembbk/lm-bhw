d1adc0ac52b932c30a89ba41ef26ee12da222b45
Artem Babak <artbbk27@gmail.com>
2023-11-20 15:27:27 +0300

    shaep

--- collate/collate.py
+++ collate/collate.py
@@ -3,9 +3,8 @@ import torch
 
 def collate_fn(data):
     sequences = [torch.tensor(pair[0], dtype=torch.int64) for pair in data]
-
-    # Если хотите объединить массивы в один longtensor, используйте torch.cat
     sequences = torch.cat(sequences)
+    print(sequences.shape)
     lengths = torch.tensor([pair[1] for pair in data])
     return sequences, lengths
 
