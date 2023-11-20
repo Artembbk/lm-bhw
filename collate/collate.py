270dc01472f85fe287e9cea95b27816fb7c744de
Artem Babak <artbbk27@gmail.com>
2023-11-20 15:00:37 +0300

    collate

--- collate/collate.py
+++ collate/collate.py
@@ -1,9 +1,10 @@
 import numpy as np
 import torch
 
-def collate_fn(arrays, numbers):
+def collate_fn(data):
+    arrays, lengths = data
     tensor_list = [torch.from_numpy(arr).to(torch.long) for arr in arrays]
 
-    numbers_tensor = torch.tensor(numbers, dtype=torch.long)
+    lengths_tensor = torch.tensor(lengths, dtype=torch.long)
     
-    return tensor_list, numbers_tensor
+    return tensor_list, lengths_tensor
