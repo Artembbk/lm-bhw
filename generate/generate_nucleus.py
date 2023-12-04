42f13817fbd28558292f7fd519605029d7a96a97
Artem Babak <dev@tararoutray.com>
2023-12-04 03:49:16 +0300

    no device for lengths

--- generate/generate_nucleus.py
+++ generate/generate_nucleus.py
@@ -24,7 +24,7 @@ def generate_nucleus(model, tokenizer, device, batch_size: int, prefix: Tensor =
         prefix[:, :] = tokenizer.token_to_id("[BOS]")
     
     while prefix.size(1) < max_len:
-        lengths = torch.full((batch_size,), prefix.size(1), device=device)
+        lengths = torch.full((batch_size,), prefix.size(1))
         logits = model(prefix, lengths)
         logits = logits[:, -1, :].squeeze(1)
         probs = F.softmax(logits, dim=-1)
