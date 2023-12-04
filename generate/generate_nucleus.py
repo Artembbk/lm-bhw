5f1ea05142d8c76836f986718b24c86c0ef83ddd
Artem Babak <dev@tararoutray.com>
2023-12-04 05:35:05 +0300

    fix nucleus

--- generate/generate_nucleus.py
+++ generate/generate_nucleus.py
@@ -37,10 +37,11 @@ def generate_nucleus(model, tokenizer, device, batch_size: int, prefix: Tensor =
             
         selected_indices = sorted_inds[below_nucleus]
         
-        # Select from reduced distribution
-        next_token = selected_indices[0] if selected_indices.numel() == 1 else torch.multinomial(torch.ones_like(probs), 1)
-        print(next_token)
-        prefix = torch.cat([prefix, next_token], dim=-1)
+        selected_probs = probs[:, selected_indices]
+        selected_probs_normalized = selected_probs / selected_probs.sum(dim=1, keepdim=True)
+        chosen_token_indices = torch.multinomial(selected_probs_normalized, 1).squeeze()
+
+        prefix = torch.cat([prefix, chosen_token_indices], dim=-1)
         
     end = torch.empty((batch_size, 1), dtype=torch.int32).to(device)
     end[:, :] = tokenizer.piece_to_id("[EOS]")
